a6cf286d7d770e0e9929c0f79d4a50c8f0091d79
Prince Igwe <igwep297@gmail.com>
2024-11-23 01:46:13 +0100

    modules, entities and services added

--- src/business-units/business-units.module.ts
+++ src/business-units/business-units.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { BusinessUnitsService } from './business-units.service';
+import { BusinessUnitsService } from './services/business-units.service';
 import { BusinessUnitsController } from './business-units.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BusinessUnit } from './entities/business.unit.entity';
+import { BusinessUnitProduct } from './entities/business.unit.product.entity';
+import { Report } from './entities/report.entity';
+import { BusinessUnitProductsService } from './services/business-unit-products.service';
 
 @Module({
-  providers: [BusinessUnitsService],
+  imports: [
+    TypeOrmModule.forFeature([
+      BusinessUnit,
+      BusinessUnitProduct,
+      Report
+    ])
+  ],
+  providers: [BusinessUnitsService, BusinessUnitProductsService],
+  exports: [BusinessUnitsService, BusinessUnitProductsService],
   controllers: [BusinessUnitsController]
 })
 export class BusinessUnitsModule {}
